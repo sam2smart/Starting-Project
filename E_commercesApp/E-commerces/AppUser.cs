ecf943870181e521431d85f9c9a3cb5831b3ef16
unknown <ajayisamuel46@gmail.com>
2026-02-21 14:11:48 +0100

    Update today

--- E_commercesApp/E-commerces/AppUser.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Ecommerces
-{
-    public class AppUser
-    {
-        public Guid Id { get; set; }
-        public string Email { get; set; }
-        public string PasswordHash { get; set; } = default!;
-    }
-}
